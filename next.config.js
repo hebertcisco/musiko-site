28a0ae8e68dda712fa8d94b97921d658653c8d91
hebertcisco <mrrabbitomg@gmail.com>
2020-11-15 19:00:13 -0300

    Project created

---
+++ next.config.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+module.exports = {
+  env: {
+    API_KEY: process.env.API_KEY,
+    FONTAWESOME: process.env.FONTAWESOME,
+    BASE_URL: process.env.BASE_URL,
+    MONGO_URL: process.env.MONGO_URL,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+  },
+};
