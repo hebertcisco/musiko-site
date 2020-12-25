64e95bbe93caa8f7ab74ce799dbdd8ac4620c66c
hebertcisco <mrrabbitomg@gmail.com>
2020-12-25 00:33:39 -0300

    Add firebase configs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -54,12 +54,39 @@ export default class CustomDocument extends Document<DocumentProps> {
   `,
     };
   }
+  runFirebase() {
+    return {
+      __html: `
+      var firebaseConfig = {
+        apiKey: ${process.env.FIREBASE_API_KEY},
+        authDomain: ${process.env.FIREBASE_AUTH_DOMAIN},
+        databaseURL: ${process.env.FIREBASE_DATABASE_URL},
+        projectId: ${process.env.FIREBASE_PROJECT_ID},
+        storageBucket: ${process.env.FIREBASE_STORAGE_BUCKET},
+        messagingSenderId: ${process.env.FIREBASE_MESSAGING_SENDER_ID},
+        appId: ${process.env.FIREBASE_APP_ID},
+        measurementId: ${process.env.FIREBASE_MEASUREMENT_ID}
+      };
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
+      `,
+    };
+  }
   public render() {
     const { styleTags } = this.props;
     return (
       <Html>
         <Head>
           {styleTags}
+          <script
+              data-ad-client={`${process.env.ADS_CLIENT}`}
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+            ></script>
+            <script src="https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js"></script>
+
+            <script src="https://www.gstatic.com/firebasejs/8.2.1/firebase-analytics.js"></script>
+            <script dangerouslySetInnerHTML={this.runFirebase()} />
           <script
             src={`https://kit.fontawesome.com/${process.env.FONTAWESOME}.js`}
           ></script>
